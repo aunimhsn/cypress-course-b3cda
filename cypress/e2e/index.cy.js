e0ba55e17a4949fa44dc3ddb7ad71624a626bf26
Aunim HASSAN <aunim.hassan@gmail.com>
2024-02-29 16:30:12 +0100

    setup: cypress

---
+++ cypress/e2e/index.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+context('Hello World!', () => {
+    beforeEach(() => {
+        cy.visit('../../app/index.html');
+    });
+
+    it('p should have text Hello World!', () => {
+        cy.get('p')
+            .should('have.text', 'Hello World!');
+    });
+})
